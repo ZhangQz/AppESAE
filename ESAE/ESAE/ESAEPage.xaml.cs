65caf283564e0330051776354b2c3710b510409a
David Prata <dparst@gmail.com>
2017-05-31 19:17:31 +0100

    BD modelo E-R actualizado
    Erro no anterior

--- ESAE/ESAE/ESAEPage.xaml.cs
+++ ESAE/ESAE/ESAEPage.xaml.cs
@@ -2,11 +2,31 @@
 
 namespace ESAE
 {
-    public partial class ESAEPage : ContentPage
+    public partial class ESAEPage : MasterDetailPage
     {
         public ESAEPage()
         {
             InitializeComponent();
         }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            
+        }
+
+		void Handle_Clicked2(object sender, System.EventArgs e)
+		{
+			
+		}
+
+		void Handle_Clicked3(object sender, System.EventArgs e)
+		{
+
+		}
+
+		void Handle_Clicked4(object sender, System.EventArgs e)
+		{
+
+		}
     }
 }

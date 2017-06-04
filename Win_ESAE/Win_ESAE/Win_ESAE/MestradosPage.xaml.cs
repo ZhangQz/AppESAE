207e600beec0be6fa93f191a702f9663ef060f96
ZhangQz <zhang773575530@hotmail.com>
2017-06-04 23:37:10 +0100

    n

---
+++ Win_ESAE/Win_ESAE/Win_ESAE/MestradosPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Win_ESAE
+{
+    public partial class MestradosPage : ContentPage
+    {
+        public MestradosPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGestaoRecursos_Clicked(object sender, EventArgs e)
+        {
+            Mestrados.GestaoRecursosPage grp = new Mestrados.GestaoRecursosPage();
+            Navigation.PushModalAsync(grp);
+        }
+
+        private void btnPsicologiaClinica_Clicked(object sender, EventArgs e)
+        {
+            Mestrados.PsicologiaClinicaPage pcp = new Mestrados.PsicologiaClinicaPage();
+            Navigation.PushModalAsync(pcp);
+        }
+
+        private void btnServicoSocial_Clicked(object sender, EventArgs e)
+        {
+            Mestrados.ServicoSocialPage ssp = new Mestrados.ServicoSocialPage();
+            Navigation.PushModalAsync(ssp);
+        }
+    }
+}
---
+++ Win_ESAE/Win_ESAE/Win_ESAE/Pos_Grads/AcunpunturaEsteticaPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Win_ESAE.Pos_Grads
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AcunpunturaEsteticaPage : ContentPage
+    {
+        public AcunpunturaEsteticaPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

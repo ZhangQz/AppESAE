8c6c21a6f1eb1e30876e73c41d8d1d3f8f0ae987
David Prata <dparst@gmail.com>
2017-06-26 18:23:02 +0100

    aleracoes no principal backoffice

--- backoffice/app/Http/Controllers/DisciplinasController.php
+++ backoffice/app/Http/Controllers/DisciplinasController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+//interfaces
+use Illuminate\Support\collection;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+//use Illuminate\Support\Facades\Input;
+
+//models
 use App\Utilizador;
 use App\Curso;
 use App\Disciplina;
@@ -11,20 +17,105 @@ use App\Evento;
 use App\User;
 use App\Propina;
 
-class DisciplinasController extends Controller
-{
-  function index() {
-    $disci = Disciplina::all();
-    return json_encode($disci);
-  }
 
-  function show($id) {
-    $disci = Disciplina::find($id);
-    return json_encode($disci);
-  }
-
+/*
   function listByCourse($id) {
     $disci = Curso::find($id)->unidades()->get();
     return json_encode($disci);
   }
+}*/
+
+class DisciplinasController extends Controller
+{
+    public function index() {
+        try {
+            $statusCode = 200; //Ok
+
+            //reset data collection
+            $response = collect([]);
+
+            //get all friends from database
+            $disciplinas = Disciplina::all();
+
+            foreach($disciplinas as $disciplina)
+            {
+                //add friend to the collection
+                $response->push([
+                    'id' => $disciplina->id,
+                    'nome' => $disciplina->nome
+                ]);
+            }
+        } catch (Exception $e) {
+            $statusCode = 400; //bad request
+        } finally {
+            return response()->json($response, $statusCode)->header('Access-Control-Allow-Origin', '*')->header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+        }
+    }
+
+    public function store(Request $dados) {
+        try {
+            $statusCode = 200;
+            $response = collect([]);
+
+            $disciplina = Disciplina::create();
+            $response->push(['created' => 'Disciplina created successfully.']);
+        } catch (Exception $e) {
+            $response->push(['error' => 'Error creating disciplina.']);
+            $statusCode = 404;
+        } finally {
+            return response()->json($response, $statusCode);
+        }
+    }
+
+    public function show($id) {
+        try {
+            $statusCode = 200;
+            $response = collect([]);
+
+            $disciplina = Disciplina::findOrFail($id);
+
+            $response->push([
+                'id' => $curso->id,
+                'nome' => $curso->nome
+            ]);
+
+        } catch (Exception $e) {
+            $response->push(['error' => 'Disciplina not found.']);
+            $statusCode = 404; //Not Found
+        } finally {
+            return response()->json($response, $statusCode)->header('Access-Control-Allow-Origin', '*')->header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+        }
+    }
+
+    public function update(Request $dados, $id) {
+        try {
+            $statusCode = 200;
+            $response = collect([]);
+
+            $disciplina = Disciplina::findOrFail($id);
+            $disciplina->fill($dados->all())->save();
+            $response->push(['updated' => 'Disciplina updated successfully.']);
+        } catch (Exception $e) {
+            $response->push(['error' => 'Error updating disciplina.']);
+            $statusCode = 404;
+        } finally {
+            return response()->json($response, $statusCode);
+        }
+    }
+
+    public function destroy($id) {
+        try {
+            $statusCode = 200;
+            $response = collect([]);
+
+            $disciplina = Disciplina::findOrFail($id);
+            $disciplina->delete();
+            $response->push(['success' => 'Disciplina deleted successfully.']);
+        } catch (Exception $e) {
+            $response->push(['error' => 'Error deleting disciplina.']);
+            $statusCode = 404;
+        } finally {
+            return response()->json($response, $statusCode);
+        }
+    }
 }
--- backoffice/app/Http/Controllers/UtilizadoresController.php
+++ backoffice/app/Http/Controllers/UtilizadoresController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+//interfaces
+use Illuminate\Support\collection;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+//use Illuminate\Support\Facades\Input;
+
+//models
 use App\Utilizador;
 use App\Curso;
 use App\Disciplina;
@@ -11,20 +17,106 @@ use App\Evento;
 use App\User;
 use App\Propina;
 
+/*
+function listByCourse($id) {
+  $users = Curso::find($id)->alunos()->get();
+  return json_encode($users);
+}
+}*/
+
 class UtilizadoresController extends Controller
 {
-    function index() {
-      $users = Utilizador::all();
-      return json_encode($users);
+    public function index() {
+        try {
+            $statusCode = 200; //Ok
+
+            //reset data collection
+            $response = collect([]);
+
+            //get all friends from database
+            $utilizadores = Utilizador::all();
+
+            foreach($utilizadores as $utilizador)
+            {
+                //add friend to the collection
+                $response->push([
+                    'id' => $utilizador->id,
+                    'numero' => $utilizador->numero,
+                    'nome' => $utilizador->nome
+                ]);
+            }
+        } catch (Exception $e) {
+            $statusCode = 400; //bad request
+        } finally {
+            return response()->json($response, $statusCode)->header('Access-Control-Allow-Origin', '*')->header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+        }
     }
 
-    function show($id) {
-      $users = Utilizador::find($id);
-      return json_encode($users);
+    public function store(Request $dados) {
+        try {
+            $statusCode = 200;
+            $response = collect([]);
+
+            $utilizador = Utilizador::create();
+            $response->push(['created' => 'Utilizador created successfully.']);
+        } catch (Exception $e) {
+            $response->push(['error' => 'Error creating utilizador.']);
+            $statusCode = 404;
+        } finally {
+            return response()->json($response, $statusCode);
+        }
     }
 
-    function listByCourse($id) {
-      $users = Curso::find($id)->alunos()->get();
-      return json_encode($users);
+    public function show($id) {
+        try {
+            $statusCode = 200;
+            $response = collect([]);
+
+            $utilizador = Utilizador::findOrFail($id);
+
+            $response->push([
+                'id' => $utilizador->id,
+                'numero' => $utilizador->ano,
+                'nome' => $utilizador->nome
+            ]);
+
+        } catch (Exception $e) {
+            $response->push(['error' => 'User not found.']);
+            $statusCode = 404; //Not Found
+        } finally {
+            return response()->json($response, $statusCode)->header('Access-Control-Allow-Origin', '*')->header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+        }
+    }
+
+    public function update(Request $dados, $id) {
+        try {
+            $statusCode = 200;
+            $response = collect([]);
+
+            $utilizador = Utilizador::findOrFail($id);
+            $utilizador->fill($dados->all())->save();
+            $response->push(['updated' => 'Utilizador updated successfully.']);
+        } catch (Exception $e) {
+            $response->push(['error' => 'Error updating utilizador.']);
+            $statusCode = 404;
+        } finally {
+            return response()->json($response, $statusCode);
+        }
+    }
+
+    public function destroy($id) {
+        try {
+            $statusCode = 200;
+            $response = collect([]);
+
+            $utilizador = Utilizador::findOrFail($id);
+            $utilizador->delete();
+            $response->push(['success' => 'Utilizador deleted successfully.']);
+        } catch (Exception $e) {
+            $response->push(['error' => 'Error deleting utilizador.']);
+            $statusCode = 404;
+        } finally {
+            return response()->json($response, $statusCode);
+        }
     }
 }
